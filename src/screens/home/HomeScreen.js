30a6769cfbacdfe73e329aaa1ee445cd7178db7b
Dantong Jin <j.dantong@gmail.com>
2020-12-31 17:08:24 +0800

    feat: updated navigation header style

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -6,7 +6,7 @@ import { TimeClock } from '../../components';
 const HomeScreen = ({ navigation }) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      title: '新闻',
+      title: 'News',
       headerRight: ({ tintColor }) => (
         <TouchableOpacity style={styles.headerButton} onPress={() => navigation.navigate('Scan')}>
           <MaterialCommunityIcons name="qrcode-scan" color={tintColor} size={22} />
--- src/screens/home/HomeStackScreen.js
+++ src/screens/home/HomeStackScreen.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
+import colors from '../../constants/colors';
 
 const HomeStack = createStackNavigator();
+
+const screenOptions = {
+  headerTintColor: colors.text,
+  headerStyle: { backgroundColor: colors.primary },
+};
+
 const HomeStackScreen = () => {
   return (
-    <HomeStack.Navigator>
+    <HomeStack.Navigator screenOptions={screenOptions}>
       <HomeStack.Screen name="Home" component={HomeScreen} />
     </HomeStack.Navigator>
   );
