62cf16a4790fc0c2c1e3363fa4b9324d2c44d4d2
Dantong Jin <j.dantong@gmail.com>
2021-01-03 20:19:50 +0800

    feat: added time clock

--- src/components/TimeClock.js
+++ src/components/TimeClock.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Text } from 'react-native';
 
-const TimeClock = () => {
-  return <Text>Time Clock</Text>;
+const TimeClock = (props) => {
+  const { style } = props;
+  const [timer, setTimer] = useState(null);
+  const [text, setText] = useState('00:00:00');
+
+  const start = useCallback(() => {
+    const now = new Date();
+    const options = { hour12: false };
+    setText(now.toLocaleTimeString('zh-CN', options));
+  }, []);
+
+  useEffect(() => {
+    setTimer(setInterval(start, 1000));
+  }, [start]);
+
+  useEffect(() => {
+    return () => clearInterval(timer);
+  }, [timer]);
+
+  return <Text style={style}>{text}</Text>;
 };
 
 export default TimeClock;
