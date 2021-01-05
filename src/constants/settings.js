5057b3fa162e0b35611f2255107dfe6b80d52cd2
Dantong Jin <j.dantong@gmail.com>
2021-01-05 14:35:59 +0800

    refactor: hidden header back button title

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -3,6 +3,7 @@ import colors from './colors';
 export const defaultScreenOptions = {
   headerTintColor: colors.text,
   headerStyle: { backgroundColor: colors.primary },
+  headerBackTitle: ' ',
 };
 
 export const agreementPdfUrl = 'https://raw.githubusercontent.com/WzDTj/example-resources/main/example.pdf';
